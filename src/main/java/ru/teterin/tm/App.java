949212d13830a58a5bba1ba1c4561a28d395c4b3
GlobalTaw <teterin2012@rambler.ru>
2020-04-21 18:11:42 +0300

    create entity

--- src/main/java/ru/teterin/tm/App.java
+++ src/main/java/ru/teterin/tm/App.java
@@ -1,4 +1,19 @@
 package ru.teterin.tm;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class App {
-}
+    public static void main(String[] args) throws IOException{
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        while(true){
+            String command=reader.readLine();
+
+            break;
+            }
+        reader.close();
+
+        }
+
+    }
