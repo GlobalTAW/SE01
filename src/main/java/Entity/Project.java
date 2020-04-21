b14e72958efa077c254a593b517306dad05bf9a4
GlobalTaw <teterin2012@rambler.ru>
2020-04-21 18:29:43 +0300

    create field and methods class Project

--- src/main/java/Entity/Project.java
+++ src/main/java/Entity/Project.java
@@ -1,4 +1,14 @@
 package Entity;
 
 public class Project {
+    private String name;
+    public Project(String name){
+        this.name = name;
+    }
+    public String getName(){
+        return this.name;
+    }
+    public void setName(String name){
+        this.name = name;
+    }
 }
