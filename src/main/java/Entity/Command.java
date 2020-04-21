b0fba10826f7bdfc6b39d80334a042f3eee46735
GlobalTaw <teterin2012@rambler.ru>
2020-04-21 18:22:41 +0300

    add command

--- src/main/java/Entity/Command.java
+++ src/main/java/Entity/Command.java
@@ -1,5 +1,15 @@
 package Entity;
 
 public enum Command {
-
+    HELP,
+    PROJECT_CLEAR,
+    PROJECT_CREATE,
+    PROJECT_LIST,
+    PROJECT_REMOVE,
+    TASK_CLEAR,
+    TASK_CREATE,
+    TASK_LIST,
+    TASK_REMOVE,
+    DEFAULT,
+    EXIT
 }
